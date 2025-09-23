377ac64fd09155166b1a01fefd269cf26888aa53
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2025-09-23 00:24:07 -0500

    fix: Update SQL and workflow for better variable handling

--- sql/test_deployment.sql
+++ sql/test_deployment.sql
@@ -1,9 +1,16 @@
 -- Test deployment SQL file
 -- Creates a test table in the DEV environment
+-- Variables will be substituted during deployment:
+--   env_db: Target database
+--   env_wh: Target warehouse
+--   env_schema: Target schema
 
-USE DATABASE &env_db;
-USE WAREHOUSE &env_wh;
-USE SCHEMA &env_schema;
+!set variable_substitution=true;
+
+USE ROLE DEV_ROLE;
+USE DATABASE IDENTIFIER(:env_db);
+USE WAREHOUSE IDENTIFIER(:env_wh);
+USE SCHEMA IDENTIFIER(:env_schema);
 
 -- Create a test table
 CREATE TABLE IF NOT EXISTS test_deployment (
