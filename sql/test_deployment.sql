4461a84872c51f3933425178eb391d8a41292530
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2025-09-23 00:21:34 -0500

    adding test script

---
+++ sql/test_deployment.sql
@@ -0,0 +1,17 @@
+-- Test deployment SQL file
+-- Creates a test table in the DEV environment
+
+USE DATABASE ${env_db};
+USE WAREHOUSE ${env_wh};
+USE SCHEMA ${env_schema};
+
+-- Create a test table
+CREATE TABLE IF NOT EXISTS test_deployment (
+    id INTEGER,
+    test_name VARCHAR(50),
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+-- Insert a test record
+INSERT INTO test_deployment (id, test_name)
+VALUES (1, 'Initial Deployment Test');
