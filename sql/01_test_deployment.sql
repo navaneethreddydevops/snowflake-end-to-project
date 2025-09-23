c9d7e01bf5bb40749a7b3c401d25c56f940a4d31
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2025-09-23 16:17:22 -0500

    Revert "adding speedup"
    This reverts commit 9567be3c55df1f6b08449ac38648f647cb9c955d.

--- sql/01_test_deployment.sql
+++ sql/01_test_deployment.sql
@@ -25,5 +25,3 @@ CREATE TABLE IF NOT EXISTS test_deployment_two (
     test_name VARCHAR(50),
     created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
 );
-
-DROP TABLE IF EXISTS test_deployment_two;
