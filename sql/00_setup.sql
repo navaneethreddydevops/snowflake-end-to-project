95a6640971c52040cdc1585ebf9847a09fa9cb55
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2025-09-23 00:25:46 -0500

    refactor: Separate setup and deployment SQL files

---
+++ sql/00_setup.sql
@@ -0,0 +1,7 @@
+-- This file handles environment setup and variable definitions
+!set variable_substitution=true;
+
+USE ROLE $env_role;
+USE DATABASE $env_db;
+USE WAREHOUSE $env_wh;
+USE SCHEMA $env_schema;
--- sql/test_deployment.sql
+++ sql/test_deployment.sql
@@ -1,16 +1,5 @@
 -- Test deployment SQL file
--- Creates a test table in the DEV environment
--- Variables will be substituted during deployment:
---   env_db: Target database
---   env_wh: Target warehouse
---   env_schema: Target schema
-
-!set variable_substitution=true;
-
-USE ROLE DEV_ROLE;
-USE DATABASE IDENTIFIER(:env_db);
-USE WAREHOUSE IDENTIFIER(:env_wh);
-USE SCHEMA IDENTIFIER(:env_schema);
+-- Creates a test table in the environment
 
 -- Create a test table
 CREATE TABLE IF NOT EXISTS test_deployment (
